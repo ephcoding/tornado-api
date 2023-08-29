3f453d071ef4dec6ded1be19a56d0fac366e5997
Eph <ephraim@modevx.com>
2023-08-29 11:26:22 -0500

    SECURITY/add security middlware

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
+const morgan = require("morgan");
+const xss = require("xss-clean");
+const express = require("express");
+const httpParamPollution = require("hpp");
+const helmetSecurityHeaders = require("helmet");
+const sanitize = require("express-mongo-sanitize");
+const expressRateLimit = require("express-rate-limit");
 require("dotenv").config({
 	path: "./config/config.env",
 });
-const express = require("express");
-const morgan = require("morgan");
-const { errorHandler } = require("./middleWare/error-handler");
-const connectDB = require("./config/database");
 
+const { errorHandler } = require("./middleWare/error-handler");
+
+const connectDB = require("./config/database");
 connectDB();
+
+const expressApp = express();
+expressApp.use(xss());
+expressApp.use(sanitize());
+expressApp.use(express.json());
+expressApp.use(expressRateLimit());
+expressApp.use(httpParamPollution());
+expressApp.use(helmetSecurityHeaders());
 
 const tornadoesRouter = require("./routes/tornadoes");
 
-const app = express();
-app.use(express.json());
-
 if ((process.env.NODE_ENV = "development")) {
-	app.use(morgan("dev"));
+	expressApp.use(morgan("dev"));
 }
 
-// TODO: add security & CORS here -- X
-
 const PORT = process.env.PORT || 5001;
-app.use("/v1/tornadoes", tornadoesRouter);
+expressApp.use("/v1/tornadoes", tornadoesRouter);
 
-app.listen(
+expressApp.listen(
 	PORT,
 	console.log(
 		`Server up in ${process.env.NODE_ENV} mode running on port ${PORT}`
 	)
 );
-
-app.use(errorHandler);
+expressApp.use(errorHandler);
 
 process.on("unhandledRejection", (error, promise) => {
 	console.log(`>> ERROR >>\n`, error.message);
