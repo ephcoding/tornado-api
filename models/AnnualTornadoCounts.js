2cd8cf958b03fbffa0e3c92be1cede525efa9c6f
Eph <ephraim@modevx.com>
2023-09-08 15:13:52 -0500

    create base tornado count schema

--- models/AnnualTornadoCounts.js
+++ models/AnnualTornadoCounts.js
@@ -1,6 +1,30 @@
 import mongoose from "mongoose";
 
-const AnnualTornadoCountsSchema = new mongoose.Schema({});
+const AnnualTornadoCountsSchema = new mongoose.Schema(
+	{
+		year: Number,
+		total_tornadoes: Number,
+		by_magnitude: [
+			{
+				magnitude: Number,
+				count: Number,
+			},
+		],
+		by_month: [
+			{
+				magnitude: Number,
+				count: Number,
+			},
+		],
+		by_state: [
+			{
+				state_abbr: String,
+				count: Number,
+			},
+		],
+	},
+	{ timestamps: true }
+);
 
 export const AnnualTornadoCounts = mongoose.model(
 	"AnnualTornadoCounts",
