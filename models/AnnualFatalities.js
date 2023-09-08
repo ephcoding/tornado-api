1ea2aebd83ffdcf9c41c80e75cd7f70715bc5962
Eph <ephraim@modevx.com>
2023-09-08 15:20:03 -0500

    create base Annual Injuries model

--- models/AnnualFatalities.js
+++ models/AnnualFatalities.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+const Decimal = mongoose.Schema.Types.Decimal128;
+
 const AnnualFatalitiesSchema = new mongoose.Schema({});
 
 export const AnnualFatalities = mongoose.model(
--- models/AnnualInjuries.js
+++ models/AnnualInjuries.js
@@ -2,7 +2,27 @@ import mongoose from "mongoose";
 
 const Decimal = mongoose.Schema.Types.Decimal128;
 
-const AnnualInjuriesSchema = new mongoose.Schema({});
+const AnnualInjuriesSchema = new mongoose.Schema({
+	total_fatalities: Number,
+	by_magnitude: [
+		{
+			magnitude: Number,
+			property_loss: Decimal,
+		},
+	],
+	by_month: [
+		{
+			month: Number,
+			property_loss: Decimal,
+		},
+	],
+	by_state: [
+		{
+			state_abbr: String,
+			property_loss: Decimal,
+		},
+	],
+});
 
 export const AnnualInjuries = mongoose.model(
 	"AnnualInjuries",
